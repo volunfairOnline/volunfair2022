f341a4358f18cfca9f17388b1cd777c1ef0108b0
albacasanovaa <alba.casanova@volunfair.org>
2021-10-30 12:06:08 +0200

    Poner Foto Premio

--- web/v-premio.php
+++ web/v-premio.php
@@ -69,9 +69,9 @@ escribe_cabecera();
 								
 						<!-- Imagen -->
 						<div class="col-md-4 wow animated fadeInDown">
-							<img src="./assets/base/img/volunfair/premio/PremioToniNadal(1).jpg" width="100%" alt="Vicente del Bosue y Xabi Torres reciben el Premio en 2019" />
+							<img src="./assets/base/img/volunfair/premio/PremioToniNadal(1).jpg" width="100%" alt="Toni Nadal recibe el Premio en 2020" />
 							<br /><br />
-							<img src="./assets/base/img/volunfair/premio/premioVicente&Xabi.jpg" width="100%" alt="Paco Arango recibe el Premio en 2018" />
+							<img src="./assets/base/img/volunfair/premio/premioVicente&Xabi.jpg" width="100%" alt="Vicente del Bosque recibe el Premio en 2019" />
 						</div>
 
 						
