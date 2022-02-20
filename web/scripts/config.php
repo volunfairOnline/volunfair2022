4ff69aac52e1a3b696c6d1bc348a7ad6115a99df
jgsamartin <jgsamartin@gmail.com>
2022-02-21 00:38:09 +0100

    Menú enlazando a v-viene

--- web/scripts/config.php
+++ web/scripts/config.php
@@ -8,7 +8,7 @@ function escribe_menu () {
 	// --- En la siguiente matriz ponemos el nombre del archivo y el nombre que queremos que aparezca en el menú superior.
 	// --- NO PONER COMA TRAS LA ÚLTIMA FILA
 	$menu = [
-		["archivo" => "v-viene-sinPonentes", 		"nombre" => "<u>Ed. 2022</u>"],
+		["archivo" => "v-viene", 		"nombre" => "<u>Ed. 2022</u>"],
 		["archivo" => "index", 			"nombre" => "Inicio"],
 		["archivo" => "v-quienes", 		"nombre" => "Equipo"],
 		["archivo" => "v-voluntario", 	"nombre" => "ONGs"],
