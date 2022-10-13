bbbeb330af23dc7237dceb1270e89854eccc3929
Beadominguez <beatriz@volunfair.org>
2022-10-13 18:47:14 +0200

    bea contacto

--- web/v-contactar.php
+++ web/v-contactar.php
@@ -55,7 +55,7 @@ escribe_cabecera();
                             	  <div class="c-content-title-1">
                                         <h3 class="c-font-uppercase c-font-bold">Para hablar con nosotros</h3>
                                         <div class="c-line-left"></div>
-                                  <p class="c-font-lowercase">Si eres ONG: 655 53 93 29 (Bea) <br/>
+                                  <p class="c-font-lowercase">Si eres ONG: 655 53 93 29 (Bea Dominguez) <br/>
                                                               Si eres medio de comunicación: 656 32 70 55 (Leonor) <br/>
                                                               Para cualquier gestión: 601 20 07 71 (María) o 638 96 56 31 (Chema)</p>
                                 </div>
