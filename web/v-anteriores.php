2390fda698c65ee755006df54479484378e11fe5
soletevendrellb <solete.vendrell.blanco@gmail.com>
2022-10-14 14:03:14 +0200

    daniinutil

--- web/v-anteriores.php
+++ web/v-anteriores.php
@@ -86,10 +86,10 @@ escribe_cabecera();
                 
                 <div class="tl-item3" onclick="location.href='v-ponentes16';">
                     
-                    <div class="tl-bg" style="background-image: url(./assets/base/img/volunfair/banderasprov/banderamadrid.png)"></div>
+                    <div class="tl-bg" style="background-image: url(./assets\base\img\volunfair\Fotos2021\carpetaanteriores\avionaco2.jpg")"></div>
                     
                     <div class="tl-year">
-                        <p class="f2 heading--sanSerif"><a href="v-ponentes16.php">Madrid</a></p>
+                        <p class="f2 heading--sanSerif"><a href="v-ponentes16.php">2016</a></p>
                     </div>
 
                     <div class="tl-content">
@@ -103,7 +103,7 @@ escribe_cabecera();
                     <div class="tl-bg" style="background-image: url(./assets/base/img/volunfair/Fotos2021/carpetaanteriores/rowing3.jpg)"></div>
                     
                     <div class="tl-year">
-                    <p class="f2 heading--sanSerif"><a href="v-ponentes17.php">Barcelona</a></p>
+                    <p class="f2 heading--sanSerif"><a href="v-ponentes17.php">2017</a></p>
                     </div>
 
                     <div class="tl-content">
